48a0320165efd18d0ea155e6f8215c9bb28c3cea
Andrew Exton <aexton@calpoly.edu>
2017-11-16 23:04:08 -0800

    mainly affinitymws.com updates

--- affinitymws.com/web/app/themes/sydney-child/footer.php
+++ affinitymws.com/web/app/themes/sydney-child/footer.php
@@ -21,8 +21,8 @@
 		
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info container">
-			<p>Proudly built with <a href="https://wordpress.org" alt="Wordpress PHP Framework">Wordpress</a></p>
-			<p>By <a href="https://affinitymws.com">Affinity Marketing + Web Solutions</a></p>
+			<p>Creating ideas with <a id="beating-heart">♥</a></p>
+			<p><a href="https://affinitymws.com">Affinity Marketing and Web Solutions, LLC</a></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 
